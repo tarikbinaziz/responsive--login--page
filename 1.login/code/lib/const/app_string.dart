4eca76e66681b308a5597c5ee3f2f1804f36a103
tarik <tba@gmail.com>
2022-08-05 13:00:51 +0600

    First commit

---
+++ 1.login/code/lib/const/app_string.dart
@@ -0,0 +1,5 @@
+
+
+class AppStrings{
+  static const title_name='Login Page';
+}
---
+++ 1.login/code/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main(){
+  runApp(loginApp());
+}
+class loginApp extends StatelessWidget {
+  const loginApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(360,690),
+      builder: ( context,  child) {
+        return MaterialApp(
+          home: ,
+        );
+      });
+      }
+  }
+
+
+
+
+
